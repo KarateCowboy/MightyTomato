81cad61b6043eb3104d0ca8811cea672b2f2079c
Matthew Origer <moriger@groupon.com>
2012-04-20 15:45:02 -0500

    Added tomato button

--- source/MightyTomato.js
+++ source/MightyTomato.js
@@ -2,15 +2,19 @@ enyo.kind({
 	name: "MightyTomato",
 	kind: enyo.VFlexBox,
 	components: [
-		{kind: enyo.PageHeader, components: [
+		{kind: enyo.PageHeader, name:"PageHeader", components: [
 			{content: "Page Header"}
 		]},
-		{flex: 1, kind: enyo.Pane, components: [
-			{flex: 1, kind: "Scroller", components: [
+		{flex: 1, kind: enyo.Pane, name: "Pane", components: [
+			{flex: 1, kind: "Scroller", name: "Scroller", components: [
+
+                {kind: enyo.IconButton, label:"Start a Tomato", onclick:"", components: [
+                    {kind: enyo.Image, src: "images/tomato.png"}
+                ]}
 				//Insert your components here
 			]}
 		]},
-		{kind: enyo.Toolbar, components: [
+		{kind: enyo.Toolbar, name: "Toolbar", components: [
 		]}
 	]
 });
--- spec/MightyTomatoSpec.js
+++ spec/MightyTomatoSpec.js
@@ -10,8 +10,20 @@ describe("MightyTomato", function () {
 
     describe("Tomato Splash Button", function () {
         it("exists", function () {
-          expect(tomato.getComponents().length).toBeGreaterThan(0);
+            expect(tomato.getComponents().length).toBeGreaterThan(0);
         });
+    });
 
+    it("displays a tomato button", function () {
+        var names = tomato.getComponents();
+        var found = false;
+        var com;
+        for ( com in names) {
+            alert(typeof names[com]);
+            if (com.getName() == "startButton") {
+                found = true;
+            }
+        }
+        expect(found).toBeTruthy();
     });
 });
